926a766f540998b4be3125e1c6c9e189b830316c
joyin1211 <54205695+joyin1211@users.noreply.github.com>
2020-12-18 23:35:45 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import logging
-
+import sqlite3
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import config
@@ -44,12 +44,27 @@ def start(update: telegram.Update, context):
 
 def help(update, context):
     update.message.reply_text(
-        'Дунаев лох.')
+        'Дунаев лох.\n\
+        /reg - зарегистрироваться в боте\n\
+        /create - создать комнату, где будет проходить игра, бот присылает id вашей комнаты\n\
+        /leave - выйти из комнаты\n\
+        /join <id комнаты> - зайти в игру с этой комнатой\n\
+        /start_game - начать игру (команда для создателя комнаты)\n\
+        /answer <слово> - дать вариант ответа\n\
+        /stats - статистика по игроку\n\
+        ')
 
 
 def create(update: telegram.Update, context):
     message = update.message
-    if (message.chat_id in is_player) and is_player[message.chat_id]:
+    con = sqlite3.connect("crocodile.db")
+    cur = con.cursor()
+    query = "SELECT * FROM users WHERE id == " + str(message.chat_id)
+    result = cur.execute(query).fetchall()
+    if not len(result):
+        message.reply_text("Вы не зарегистрированы.")
+        return
+    if (message.chat_id in is_player) and is_player[message.chat_id][0]:
         message.reply_text("Вы не можете создать новую комнату, находясь уже в комнате")
         return
     room = generate_id(LEN)
@@ -64,14 +79,40 @@ def create(update: telegram.Update, context):
     bot.send_message(update.message.chat_id, "Айди вашей комнаты: " + room)
 
 
+def reg(update, context):
+    msg = update.message
+    con = sqlite3.connect("crocodile.db")
+    cur = con.cursor()
+    query = "SELECT * FROM users WHERE id == {}".format(msg.chat_id)
+    result = cur.execute(query).fetchall()
+    if len(result):
+        msg.reply_text("Вы уже зарегистрированы")
+        return
+    query = "INSERT INTO users VALUES ({}, '{}', 1, 0)".format(msg.chat_id, msg.from_user.username)
+    result = cur.execute(query).fetchall()
+    con.commit()
+    msg.reply_text("Вы успешно зарегистрировались!")
+
+
 def join(update, context):
     msg: telegram.Message = update.message
-    room = msg.text.split()[1]
-    is_player[msg.chat_id] = (True, room)
-    rooms[room].append((msg.chat_id, msg.from_user.username))
-    print(rooms)
-    print(is_player)
-    msg.reply_text("Вы успешно вошли в комнату!")
+    try:
+        con = sqlite3.connect("crocodile.db")
+        cur = con.cursor()
+        query = "SELECT * FROM users WHERE id == " + str(msg.chat_id)
+        result = cur.execute(query).fetchall()
+        if not len(result):
+            msg.reply_text("Вы не зарегистрированы.")
+            return
+        if (msg.chat_id in is_player) and is_player[msg.chat_id][0]:
+            msg.reply_text("Вы не можете войти в другую комнату, находясь уже в комнате")
+            return
+        room = msg.text.split()[1]
+        is_player[msg.chat_id] = (True, room)
+        rooms[room].append((msg.chat_id, msg.from_user.username))
+        msg.reply_text("Вы успешно вошли в комнату!")
+    except Exception:
+        msg.reply_text("Произошла ошибка. Скорее всего вы неверно ввели номер комнаты")
 
 
 def leave(update, context):
@@ -96,23 +137,42 @@ def leave(update, context):
 
 def answer(update, context):
     msg: telegram.Message = update.message
-    if not is_player[msg.chat_id][0]:
+    con = sqlite3.connect("crocodile.db")
+    cur = con.cursor()
+    if (msg.chat_id not in is_player) or (is_player[msg.chat_id][0] == False):
+        msg.reply_text("Вы еще не вошли в игру")
         return
     room = is_player[msg.chat_id][1]
-    answ = msg.text.split()[1]
-    if answ != keywords[room]:
-        msg.reply_text("К сожалению, вы не отгадали слово. Попробуйте еще")
-    else:
-        msg.reply_text("Вы угадали слово, поздравляю! Админ решит, начинать ли новый раунд.")
-        players = rooms[room]
-        for player in players:
-            if player != (msg.chat_id, msg.from_user.username):
-                bot.send_message(player[0], "Слово было угадано. Админ решит судьбу этой комнаты")
-        is_started[room] = False
+    if guessers[room][0] == msg.chat_id:
+        msg.reply_text("Вы ведущий, так что не можете отгадывать слова")
+        return
+    try:
+        answ = msg.text.split()[1]
+        if answ != keywords[room]:
+            msg.reply_text("К сожалению, вы не отгадали слово. Попробуйте еще")
+        else:
+            query = "SELECT wins FROM users WHERE id == {}".format(msg.chat_id)
+            result = cur.execute(query).fetchall()
+            query = "UPDATE users SET wins = {} WHERE id == {}".format(result[0][0] + 1, msg.chat_id)
+            result = cur.execute(query)
+            con.commit()
+            msg.reply_text("Вы угадали слово, поздравляю! Админ решит, начинать ли новый раунд.")
+            players = rooms[room]
+            for player in players:
+                if player != (msg.chat_id, msg.from_user.username):
+                    bot.send_message(player[0], "Слово было угадано. Админ решит судьбу этой комнаты")
+            is_started[room] = False
+    except Exception:
+        msg.reply_text("Произошла ошибка. Скорее всего вы не ответ.")
 
 
 def Start(update, context):
     msg: telegram.Message = update.message
+    con = sqlite3.connect("crocodile.db")
+    cur = con.cursor()
+    if (msg.chat_id not in is_player) or (is_player[msg.chat_id][0] == False):
+        msg.reply_text("Вы еще не вошли в игру")
+        return
     room = is_player[msg.chat_id][1]
     if admins[room] != (msg.chat_id, msg.from_user.username):
         msg.reply_text("Вы не админ, чтобы начинать игру")
@@ -122,6 +182,11 @@ def Start(update, context):
     guessers[room] = players[guesser]
     is_started[room] = True
     for player in players:
+        query = "SELECT games FROM users WHERE id == {}".format(player[0])
+        result = cur.execute(query).fetchall()
+        query = "UPDATE users SET games = {} WHERE id == {}".format(result[0][0] + 1, player[0])
+        result = cur.execute(query)
+        con.commit()
         if player == players[guesser]:
             word = random.choice(words)
             keywords[room] = word
@@ -129,6 +194,21 @@ def Start(update, context):
         else:
             bot.send_message(player[0], "Вы игрок! Вы должны угадать загаданное ведущим слово")
     print(players)
+
+
+def stats(update, context):
+    msg: telegram.Message = update.message
+    con = sqlite3.connect("crocodile.db")
+    cur = con.cursor()
+    query = "SELECT * FROM users WHERE id == {}".format(msg.chat_id)
+    result = cur.execute(query).fetchall()[0]
+    if len(result) == 0:
+        msg.reply_text("Вы не зарегистрированы")
+        return
+    message = "Статистика игрока " + result[1] + '\n'
+    message += "Количество игр: " + str(result[2]) + '\n'
+    message += "Количество побед: " + str(result[3])
+    msg.reply_text(message)
 
 
 def error(update, context):
@@ -144,22 +224,12 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("start_game", Start))
     dp.add_handler(CommandHandler("create", create))
+    dp.add_handler(CommandHandler("reg", reg))
     dp.add_handler(CommandHandler("leave", leave))
+    dp.add_handler(CommandHandler("stats", stats))
     dp.add_handler(CommandHandler("join", join))
     dp.add_handler(CommandHandler("answer", answer))
     dp.add_error_handler(error)
-    '''
-    if config.HEROKU_APP_NAME is None:
-        # Start the Bot
-        # Run the bot until you press Ctrl-C or the process receives SIGINT,
-        #  SIGTERM or SIGABRT. This should be used most of the time, since
-        # start_polling() is non-blocking and will stop the bot gracefully.
-    else:
-        updater.start_webhook(listen="0.0.0.0",
-                              port=config.PORT,
-                              url_path=config.TOKEN)
-        updater.bot.set_webhook(f"https://{config.HEROKU_APP_NAME}.herokuapp.com/{config.TOKEN}")
-        '''
     updater.start_polling()
     updater.idle()
 
